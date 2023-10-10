a51499b7696452da3348b6b36dd16490dc6a8eab
DayronBG <139827189+DayronbermudezG@users.noreply.github.com>
2023-10-10 03:02:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,20 @@ echo "/_/  \__/_/ /_/_/_/\_,_//_\_\/_/  \___/\___/_/  "
 echo "            Creado por DayronBG"
 echo ""
 echo ""
+
 pkg install clang
+
 g++ -o TermuxTool dbg/TermuxTool.cpp
+
 rm -r dbg
 rm README.md
 rm LICENSE
+
 echo "cd .. && rm -r TermuxTool/">uninstall.sh
+
 chmod +x uninstall.sh
+
 rm install.sh
+
 clear
 ./TermuxTool
